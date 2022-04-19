7f4063e20df4968a158921ae81be00c0c2a4c545
pooetitu <pooetitu@gmail.com>
2022-04-20 01:52:31 +0200

    [CC-67]: Add icons, empty fragments, empty navigation graph

---
+++ app/src/main/java/com/cloudcoding/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.cloudcoding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cloudcoding.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
