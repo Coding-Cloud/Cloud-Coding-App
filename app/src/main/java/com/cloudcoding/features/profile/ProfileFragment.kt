b43314f5605e75f6e19113667afc0ceabeae2f7e
pooetitu <pooetitu@gmail.com>
2022-04-26 15:00:36 +0200

    [CC-67]: add profile page

---
+++ app/src/main/java/com/cloudcoding/features/profile/ProfileFragment.kt
@@ -0,0 +1,41 @@
+package com.cloudcoding.features.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.cloudcoding.R
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.profile_fragment.*
+
+class ProfileFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        parent: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.profile_fragment, parent, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        activity?.onBackPressedDispatcher?.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    findNavController().popBackStack()
+                }
+            })
+        viewpager.adapter = ProfileAdapter(this)
+        TabLayoutMediator(tabLayout, viewpager) { tab: TabLayout.Tab, i: Int ->
+            println(i)
+            when (i) {
+                0 -> tab.text = "Posts"
+                else -> tab.text = "Groups"
+            }
+        }.attach()
+    }
+}
